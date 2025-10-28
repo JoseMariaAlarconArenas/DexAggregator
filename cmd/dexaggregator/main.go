d673b81d2af726ae0109b0b8f462caeb52703be0
Jose <jose_alarcon_20@hotmail.com>
2025-10-28 20:30:59 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/dexaggregator/main.go
@@ -0,0 +1,23 @@
+// cmd/dexaggregator/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "dexaggregator/internal/dexaggregator"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := dexaggregator.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
